1f3d32014c60d316943dc15edac7dd4280332d6b
biareichert <beatrizreichert99@gmail.com>
2019-05-29 11:06:20 -0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -5,7 +5,7 @@ from mininet.cli import CLI
 def mycmd( self, line ):
     "mycmd is an example command to extend the Mininet CLI"
     net = self.mn
-    output( 'mycmd invoked for', net, 'with line', line, '\n'  )
+    "mycmd invoked for", net, "with line", line, "\n"
 CLI.do_mycmd = mycmd
 
 net = Mininet(SingleSwitchTopo(2))
