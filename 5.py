b5104f86db605a664c2488aac33649ed4c3d8de3
biareichert <beatrizreichert99@gmail.com>
2019-05-30 15:33:42 -0300

    Delete 5.py

--- 5.py
+++
@@ -1,5 +0,0 @@
-def mycmd( self, line ):
-    "mycmd is an example command to extend the Mininet CLI"
-    net = self.mn
-    output( 'mycmd invoked for', net, 'with line', line, '\n'  )
-CLI.do_mycmd = mycmd
